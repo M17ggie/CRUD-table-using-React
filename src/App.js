a5a32ae825461f4b3fe2066f19ce81a165e5a2fe
M17ggie <102402171+M17ggie@users.noreply.github.com>
2022-06-19 17:44:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import NewOrderForm from "./components/NewOrderForm";
+import OrderList from "./components/OrderList";
+
+const data = [
+  {
+    id: 1,
+    name: 'Black cat',
+    age: 45,
+    email: 'black@blackie.com',
+    designation: 'Web Developer'
+  },
+  {
+    id: 2,
+    name: 'Raj',
+    age: 17,
+    email: 'raj@raju.com',
+    designation: 'Front-end Developer'
+  },
+  {
+    id: 3,
+    name: 'Danny',
+    age: 23,
+    email: 'danny@daniella.com',
+    designation: 'Cyber security'
+  },
+]
+
+function App() {
+
+  const [detail, setDetail] = useState(data)
+  const [showModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => {
+    setShowModal(true)
+  }
+
+  const hideModalHandler = () => {
+    setShowModal(false)
+  }
+
+  const getDataHandler = (enteredData) => {
+    setDetail(prevData => [...prevData, enteredData]);
+  }
+
+  const deleteRowHandler = (rowId) => {
+    setDetail(prevData => prevData.filter(order => order.id != rowId))
+  }
+
+  return (
+    <div className="App">
+      <OrderList data={detail} onDelete={deleteRowHandler} />
+      <button onClick={showModalHandler}>Add</button>
+      {showModal && <NewOrderForm onGetNewData={getDataHandler} onClose={hideModalHandler} />}
+    </div>
+  )
+}
+
+export default App;
